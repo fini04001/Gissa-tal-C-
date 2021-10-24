39ccb9d85d7078325caea0f6bbc4b34210d970c5
fini04001 <92377261+fini04001@users.noreply.github.com>
2021-10-24 15:58:30 +0200

    Add files via upload

---
+++ gissa tal C#/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tal_gissning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+            int GenereratTal = slump.Next(100);
+            int gissning = 101;
+            int antalFörsök = 0;
+
+
+            while(gissning != slumpTal)
+            {
+                Console.WriteLine("Guess the generated between 1 - 100 number!");
+                gissning = int.Parse(Console.ReadLine());
+                antalFörsök ++;
+
+                if(gissning > GenereratTal){
+                    Console.WriteLine("Too high");
+                }
+                else if(gissning < GenereratTal){
+                    Console.WriteLine("Too low");
+                }
+                else{
+                    Console.WriteLine("Correct! You have won!");
+                    Console.WriteLine("You answered in: "+antalFörsök+" attempts");
+                }
+            }
+        }
+    }
+}
